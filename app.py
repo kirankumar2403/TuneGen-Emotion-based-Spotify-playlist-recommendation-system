5405b317a767bdcc095ac271a3e4bd6053e1cc9e
kirankumar2403 <kirannallagutla24@gmail.com>
2025-07-06 12:15:16 +0530

    first commit

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def get_playlist_url(emotion):
     return playlists.get(emotion)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 10000))  # fallback for local
+    app.run(host='0.0.0.0', port=port, debug=True)
